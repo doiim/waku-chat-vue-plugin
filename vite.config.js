cad2c7c9c3481602f0f6216d0248c2abd7bf4623
raprogue <guilherme@doiim.com>
2024-03-15 17:56:25 -0300

    changed css imports on vite added .d.ts generation

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
     plugins: [
         vue(),
+        libInjectCss(),
+        dts({ include: ['src'] })
     ],
+    css: {
+        modules: { scopeBehaviour: 'global', },
+    },
     input: 'src/index.ts',
-    base: '/waku-chat-vue-plugin/',
+    base: '/waku-chat-vue/',
     resolve: {
         preserveSymlinks: true,
     },
@@ -15,12 +22,11 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, './src/index.ts'),
             name: 'ChatComponent',
-            fileName: 'waku-chat-vue-plugin',
+            fileName: 'waku-chat-vue',
         },
         rollupOptions: {
             external: ['vue'],
             output: {
-                intro: `require ('./style.css')`,
                 globals: {
                     vue: 'Vue',
                 },

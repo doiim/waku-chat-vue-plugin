dec7e97256d1821ac85f3a58c53f4a2e451be0fe
raprogue <guilherme@doiim.com>
2024-03-29 17:00:20 -0300

    improving style

--- src/components/WakuChat.vue
+++ src/components/WakuChat.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted, watchEffect, onBeforeUnmount, defineProps } from "vue";
-import { Message } from "../types/ChatTypes";
+import { Message, WakuChatConfigCss } from "../types/ChatTypes";
 import {
   sendMessage,
   loadChat,
@@ -26,18 +26,75 @@ const isChatOpen = ref<boolean>(false);
 const messageFiltered = ref<Message[]>([]);
 const messageInput = ref<string>('');
 
-const computedCss = ref<any>({
-  primaryColor: 'rgba(29, 78, 216, 1)',
-  primaryColorHover: 'rgba(29, 78, 180, 1)',
-  primaryTextColor: 'rgba(255, 255, 255, 1)',
-  secondaryColor: 'rgba(229, 231, 235, 1)',
-  secondaryColorHover: 'rgba(229, 231, 235, 0.7)',
-  secondaryTextColor: 'rgba(0, 0, 0, 1)',
-  backgroundColor: 'rgba(0, 0, 0, 0.05)',
-  myMessageColor: 'rgba(29, 78, 216, 1)',
-  myMessageTextColor: 'rgba(255, 255, 255, 1)',
-  otherMessageColor: 'rgba(136, 153, 166, 0.3)',
-  otherMessageTextColor: 'rgba(29, 78, 216, 1)',
+const computedCss = ref<WakuChatConfigCss>({
+  colors: {
+    header: {
+      main: 'rgba(219, 234, 254, 1)',
+      text: 'rgba(107, 114, 128, 1)',
+      textHover: 'rgba(37, 99, 235, 1)',
+    },
+    room: {
+      btn: {
+        text: 'rgba(31, 41, 55, 1)',
+        textHover: 'rgba(37, 99, 235, 1)',
+      },
+      dropdown: {
+        main: 'rgba(255, 255, 255, 1)',
+        text: 'rgba(31, 41, 55, 1)',
+        hover: 'rgba(243, 244, 246, 1)',
+        selected: 'rgba(29, 78, 216, 1)'
+      }
+    },
+    subHeader: {
+      main: 'rgba(239, 246, 255, 1)',
+      text: 'rgba(37, 99, 235, 1)',
+      textHover: 'rgba(37, 99, 235, 1)',
+    },
+    loadBtn: {
+      main: 'rgba(37, 99, 235, 1)',
+      hover: 'rgba(59, 130, 246,1)',
+      text: 'rgba(249, 250, 251, 1)',
+      textHover: 'rgba(37, 99, 235, 1)',
+    },
+    loadingBtn: {
+      main: 'rgba(37, 99, 235, 1)',
+      text: 'rgba(249, 250, 251, 1)',
+    },
+    openBtn: {
+      main: 'rgba(37, 99, 235, 1)',
+      hover: 'rgba(59, 130, 246,1)',
+      text: 'rgba(249, 250, 251, 1)',
+      textHover: 'rgba(37, 99, 235, 1)',
+    },
+    sendBtn: {
+      main: 'rgba(37, 99, 235, 1)',
+      hover: 'rgba(59, 130, 246,1)',
+      text: 'rgba(249, 250, 251, 1)',
+      textHover: 'rgba(37, 99, 235, 1)',
+    },
+    input: {
+      main: 'rgba(229, 231, 235, 1)',
+      placeholder: 'rgba(156, 163, 175, 1)',
+      text: 'rgba(31, 41, 55, 1)'
+    },
+    minimizeBtn: {
+      main: 'rgba(107, 114, 128, 1)',
+      hover: 'rgba(37, 99, 235, 1)',
+    },
+    chat: {
+      myMessage: {
+        main: 'rgba(37, 99, 235, 1)',
+        text: 'rgba(249, 250, 251, 1)',
+      },
+      otherMessage: {
+        main: 'rgba(229, 231, 235, 1)',
+        text: 'rgba(31, 41, 55, 1)',
+      },
+      timestamp: 'rgba(156, 163, 175, 1)',
+    },
+    background: 'rgba(249, 250, 251, 1)',
+    border: 'rgba(37, 99, 235, 1)',
+  }
 });
 
 const editMode = ref(false);
@@ -129,31 +186,26 @@ watchEffect(() => {
     scrollToBottom();
 });
 
+function mergeObjects(target: any, source: any) {
+  for (const key in source) {
+    if (source[key] instanceof Object) {
+      if (!(target[key] instanceof Object)) {
+        target[key] = {};
+      }
+      mergeObjects(target[key], source[key]);
+    } else {
+      target[key] = source[key];
+    }
+  }
+}
+
 watchEffect(() => {
   const options = getOptions();
-  const cssConfig = options?.cssConfig as Record<string, string> | undefined;;
+  const colorConfig = options?.cssConfig as Record<string, any> | undefined;;
 
-  if (!cssConfig) return;
+  if (!colorConfig) return;
 
-  const cssProperties = [
-    'primaryColor',
-    'primaryColorHover',
-    'primaryTextColor',
-    'secondaryColor',
-    'secondaryColorHover',
-    'secondaryTextColor',
-    'backgroundColor',
-    'otherMessageColor',
-    'otherMessageTextColor',
-    'myMessageColor',
-    'myMessageTextColor',
-  ];
-
-  cssProperties.forEach((property) => {
-    if (cssConfig[property]) {
-      computedCss.value[property] = cssConfig[property];
-    }
-  });
+  mergeObjects(computedCss.value, colorConfig);
 });
 
 </script>
@@ -162,23 +214,12 @@ watchEffect(() => {
   <div v-if="getStatus() === 'connected'">
     <div v-if="isChatOpen" class="chat-container" :class="{ 'open': isChatOpen }">
       <div class="chat-header">
-        <div class="user-section">
-          <div v-if="getOptions()?.changeNickMode === 'interface'" class="user-profile" @click="enterEditMode">
-            <span v-if="!editMode">{{ getMyName() }}</span>
-            <input v-model="editedUserName" v-else @blur="exitEditMode" @keypress.enter="saveEditedUserName"
-              class="edit-user-input" />
-          </div>
-          <div v-else class="user-profile non-edit">
-            <span>{{ getMyName() }}</span>
-          </div>
-          <button @click="closeChat" class="minimize-button">-</button>
-        </div>
         <div class="room-section">
           <div class="room-info">
-            <span class="room-name">{{ getRoomName(getRoom()) + ' room' }}</span>
+            Room
           </div>
           <div class="room-dropdown">
-            <button class="dropdown-button">Change Room</button>
+            <button class="dropdown-button">{{ getRoomName(getRoom()) }}</button>
             <div class="dropdown-content">
               <div v-for="availableRoom in getOptions()?.availableRooms" :key="availableRoom">
                 <button @click="changeRoomDropdown(availableRoom)">
@@ -188,18 +229,30 @@ watchEffect(() => {
             </div>
           </div>
         </div>
+        <div class="settings-section">
+          <div>Settings</div>
+          <button @click="closeChat" class="minimize-button">-</button>
+        </div>
+      </div>
+      <div class="chat-subHeader">
+        <div class="user-section">
+          <div v-if="getOptions()?.changeNickMode === 'interface'" class="user-profile" @click="enterEditMode">
+            <span v-if="!editMode">{{ getMyName() }}</span>
+            <input v-model="editedUserName" v-else @blur="exitEditMode" @keypress.enter="saveEditedUserName"
+              class="edit-user-input" />
+          </div>
+          <div v-else class="user-profile non-edit">
+            <span>{{ getMyName() }}</span>
+          </div>
+        </div>
       </div>
       <div class="chat-body" ref="messageContainerRef">
         <div v-for="message in messageFiltered" :key="message.id"
           :class="{ 'own-message': message.author.id === getMyID() }" class="message-container">
+          <span class="user-name-baloon">
+            {{ message.author.name }}
+          </span>
           <div class="message">
-            <div class="message-info">
-              <span>
-                <span class="user-name-baloon">
-                  {{ message.author.name }}
-                </span>
-              </span>
-            </div>
             <div class="message-content">{{ message.data.text }}</div>
           </div>
           <div class="timestamp">
@@ -239,7 +292,7 @@ watchEffect(() => {
   display: none;
   right: 0;
   position: absolute;
-  background-color: v-bind('computedCss.primaryColor');
+  background-color: v-bind('computedCss.colors.room.dropdown.main');
   min-width: 100%;
   z-index: 1;
   max-width: 256px;
@@ -247,11 +300,10 @@ watchEffect(() => {
   text-overflow: ellipsis;
   white-space: nowrap;
   border-radius: 16px;
-  border: 2px solid v-bind('computedCss.secondaryColor');
 }
 
 .dropdown-content button {
-  color: v-bind('computedCss.primaryTextColor');
+  color: v-bind('computedCss.colors.room.dropdown.text');
   padding: 12px 16px;
   text-decoration: none;
   display: block;
@@ -265,7 +317,7 @@ watchEffect(() => {
 }
 
 .dropdown-content button:hover {
-  background-color: v-bind('computedCss.primaryColorHover');
+  background-color: v-bind('computedCss.colors.room.dropdown.hover');
 }
 
 .dropdown-button {
@@ -273,7 +325,11 @@ watchEffect(() => {
   border: none;
   cursor: pointer;
   font-size: 16px;
-  color: v-bind('computedCss.primaryTextColor');
+  color: v-bind('computedCss.colors.room.btn.text');
+}
+
+.dropdown-button:hover {
+  color: v-bind('computedCss.colors.room.btn.textHover');
 }
 
 .chat-container {
@@ -282,8 +338,8 @@ watchEffect(() => {
   position: fixed;
   bottom: 16px;
   right: 16px;
-  background-color: v-bind('computedCss.secondaryColor');
-  border: 2px solid v-bind('computedCss.primaryColor');
+  background-color: v-bind('computedCss.colors.background');
+  border: 2px solid v-bind('computedCss.colors.border');
   border-radius: 16px;
   display: flex;
   flex-direction: column;
@@ -295,15 +351,18 @@ watchEffect(() => {
 }
 
 .chat-header {
-  background-color: v-bind('computedCss.primaryColor');
-  color: v-bind('computedCss.primaryTextColor');
+  background-color: v-bind('computedCss.colors.header.main');
+  color: v-bind('computedCss.colors.header.text');
   padding: 10px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   border-top-left-radius: 16px;
   border-top-right-radius: 16px;
-  flex-direction: column;
+}
+
+.settings-section {
+  display: flex;
 }
 
 .user-section {
@@ -316,25 +375,16 @@ watchEffect(() => {
 .room-section {
   display: flex;
   width: 100%;
-  justify-content: space-between;
 }
 
-.user-profile {
+.chat-subHeader {
   display: flex;
   align-items: center;
   height: 48px;
-  border: 2px solid v-bind('computedCss.secondaryColor');
   padding: 0px 8px;
   width: 100%;
-  margin-right: 8px;
-  cursor: pointer;
-  background-color: v-bind('computedCss.primaryColor');
-  transition: background-color 0.3s ease-in-out;
-  border-radius: 16px;
-}
-
-.user-profile:hover {
-  background-color: v-bind('computedCss.primaryColorHover');
+  background-color: v-bind('computedCss.colors.subHeader.main');
+  color: v-bind('computedCss.colors.subHeader.text');
 }
 
 .non-edit {
@@ -343,7 +393,6 @@ watchEffect(() => {
 
 .edit-user-input {
   font-size: 14px;
-  border: 1px solid v-bind('computedCss.primaryColor');
   border-radius: 16px;
   margin: 4px 0px;
   height: 38px;
@@ -363,19 +412,12 @@ watchEffect(() => {
   flex: 1;
   overflow-y: auto;
   padding: 10px;
-  background-color: v-bind('computedCss.backgroundColor');
-}
-
-.message-info {
-  display: flex;
-  align-items: center;
 }
 
 .chat-footer {
   display: flex;
   align-items: center;
   padding: 16px;
-  background-color: v-bind('computedCss.backgroundColor');
 }
 
 .message-input {
@@ -383,9 +425,8 @@ watchEffect(() => {
   flex: 1;
   height: 32px;
   padding: 16px;
-  border: 2px solid v-bind('computedCss.primaryColor');
-  color: v-bind('computedCss.primaryColor');
-  background-color: v-bind('computedCss.backgroundColor');
+  color: v-bind('computedCss.colors.input.text');
+  background-color: v-bind('computedCss.colors.input.main');
   border-radius: 16px;
 }
 
@@ -395,10 +436,7 @@ watchEffect(() => {
 .send-button {
   width: 64px;
   height: 64px;
-  background-color: v-bind('computedCss.primaryColor');
-  color: v-bind('computedCss.primaryTextColor');
   border-radius: 50%;
-  border: 2px solid v-bind('computedCss.secondaryColor');
   transition: background-color 0.3s ease-in-out;
   display: flex;
   justify-content: center;
@@ -411,12 +449,6 @@ watchEffect(() => {
   cursor: pointer;
 }
 
-.open-button:hover,
-.minimize-button:hover,
-.send-button:hover {
-  background-color: v-bind('computedCss.primaryColorHover');
-}
-
 .open-button,
 .spinner {
   position: fixed;
@@ -424,23 +456,62 @@ watchEffect(() => {
   bottom: 36px;
 }
 
+.load-button {
+  background-color: v-bind('computedCss.colors.loadBtn.main');
+  color: v-bind('computedCss.colors.loadBtn.text');
+}
 
-.spinner div {
-  width: 16px;
-  height: 16px;
-  border: 4px solid v-bind('computedCss.secondaryColor');
-  border-top: 4px solid transparent;
-  border-radius: 50%;
-  animation: spin 1s linear infinite;
+.load-button:hover {
+  background-color: v-bind('computedCss.colors.loadBtn.hover');
+  color: v-bind('computedCss.colors.loadBtn.textHover');
+}
+
+.open-button {
+  background-color: v-bind('computedCss.colors.openBtn.main');
+  color: v-bind('computedCss.colors.openBtn.text');
+}
+
+.open-button:hover {
+  background-color: v-bind('computedCss.colors.openBtn.hover');
+  color: v-bind('computedCss.colors.openBtn.textHover');
+}
+
+.spinner {
+  background-color: v-bind('computedCss.colors.loadingBtn.main');
+  color: v-bind('computedCss.colors.loadingBtn.text');
 }
 
 .minimize-button {
   width: 32px;
   height: 32px;
+  background-color: v-bind('computedCss.colors.minimizeBtn.main');
+}
+
+.minimize-button:hover {
+  background-color: v-bind('computedCss.colors.minimizeBtn.hover');
+}
+
+.send-button {
+  background-color: v-bind('computedCss.colors.sendBtn.main');
+  color: v-bind('computedCss.colors.sendBtn.text');
+}
+
+.send-button:hover {
+  background-color: v-bind('computedCss.colors.sendBtn.hover');
+  color: v-bind('computedCss.colors.sendBtn.textHover');
+}
+
+.spinner div {
+  width: 16px;
+  height: 16px;
+  border: 4px solid v-bind('computedCss.colors.loadingBtn.text');
+  border-top: 4px solid transparent;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
 }
 
 .own-message .user-name-baloon {
-  color: v-bind('computedCss.myMessageTextColor');
+  color: v-bind('computedCss.colors.chat.myMessage.main');
 }
 
 .own-message div {
@@ -448,8 +519,8 @@ watchEffect(() => {
 }
 
 .own-message .message {
-  background-color: v-bind('computedCss.myMessageColor');
-  color: v-bind('computedCss.myMessageTextColor');
+  background-color: v-bind('computedCss.colors.chat.myMessage.main');
+  color: v-bind('computedCss.colors.chat.myMessage.text');
   align-self: end;
 }
 
@@ -457,21 +528,12 @@ watchEffect(() => {
   align-self: end;
 }
 
-.user-name-baloon {
-  font-weight: bold;
-  width: 100%;
-  margin-left: 4px;
-  text-align: start;
-  color: v-bind('computedCss.primaryColor');
+.own-message .user-name-baloon {
+  align-self: end;
 }
 
-.user-name-baloon-btn {
-  background: none;
-  border: none;
-  color: v-bind('computedCss.secondaryColor');
-  cursor: pointer;
-  font-weight: bold;
-  text-decoration: underline;
+.user-name-baloon {
+  color: v-bind('computedCss.colors.chat.otherMessage.main');
 }
 
 .message-container {
@@ -490,13 +552,13 @@ watchEffect(() => {
   max-width: 67%;
   padding: 10px;
   border-radius: 8px;
-  background-color: v-bind('computedCss.otherMessageColor');
-  color: v-bind('computedCss.otherMessageTextColor');
+  background-color: v-bind('computedCss.colors.chat.otherMessage.main');
+  color: v-bind('computedCss.colors.chat.otherMessage.text');
 }
 
 .timestamp {
   font-size: 12px;
-  color: v-bind('computedCss.primaryColor');
+  color: v-bind('computedCss.colors.chat.timestamp');
 }
 
 .message-content {
--- src/types/ChatTypes.ts
+++ src/types/ChatTypes.ts
@@ -16,17 +16,76 @@ export interface WakuChatVuePluginOptions {
     availableRooms: string[],
     changeNickMode: 'message' | 'interface' | 'none',
     allowPrivateChat: boolean,
-    cssConfig?: {
-        primaryColor?: string,
-        primaryColorHover?: string,
-        primaryTextColor?: string,
-        secondaryColor?: string,
-        secondaryColorHover?: string,
-        secondaryTextColor?: string,
-        backgroundColor?: string,
-        myMessageColor?: string,
-        myMessageTextColor?: string,
-        otherMessageColor?: string,
-        otherMessageTextColor?: string,
+    cssConfig?: WakuChatConfigCss
+}
+
+export interface WakuChatConfigCss {
+    colors: {
+        header: {
+            main: string,
+            text: string,
+            textHover: string,
+        },
+        room: {
+            btn: {
+                text: string,
+                textHover: string,
+            },
+            dropdown: {
+                main: string,
+                text: string,
+                hover: string,
+                selected: string,
+            }
+        },
+        subHeader: {
+            main: string,
+            text: string,
+            textHover: string,
+        },
+        loadBtn: {
+            main: string,
+            hover: string,
+            text: string,
+            textHover: string,
+        },
+        loadingBtn: {
+            main: string,
+            text: string,
+        },
+        openBtn: {
+            main: string,
+            hover: string,
+            text: string,
+            textHover: string,
+        },
+        sendBtn: {
+            main: string,
+            hover: string,
+            text: string,
+            textHover: string,
+        },
+        input: {
+            main: string,
+            placeholder: string,
+            text: string,
+        },
+        minimizeBtn: {
+            main: string,
+            hover: string,
+        },
+        chat: {
+            myMessage: {
+                main: string,
+                text: string,
+            },
+            otherMessage: {
+                main: string,
+                text: string,
+            },
+            timestamp: string,
+        },
+        background: string,
+        border: string,
     }
 }

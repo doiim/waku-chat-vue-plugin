41b2fcff19af8b29914c1aa6d200620fce22dfaa
raprogue <guilherme@doiim.com>
2024-04-19 17:35:22 -0300

    generating types

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import fs from 'fs'
 
 export default defineConfig({
     plugins: [
         vue(),
-        dts({ include: ['src'] })
+        dts({
+            include: ['src'], outputDir: 'dist', afterBuild: (files) => {
+                const srcPath = resolve(__dirname, 'dist/src/index.d.ts')
+                const destPath = resolve(__dirname, 'dist/waku-chat-vue-plugin.d.ts')
+                fs.renameSync(srcPath, destPath)
+            }
+        })
     ],
     css: {
         modules: { scopeBehaviour: 'global', },
-    },
-    input: 'src/index.ts',
-    base: '/waku-chat-vue-plugin/',
-    resolve: {
-        preserveSymlinks: true,
     },
     build: {
         lib: {
@@ -23,13 +25,13 @@ export default defineConfig({
             fileName: 'waku-chat-vue-plugin',
         },
         rollupOptions: {
-            external: ['vue','protobufjs','@waku/sdk','@libp2p/bootstrap'],
+            external: ['vue', 'protobufjs', '@waku/sdk', '@libp2p/bootstrap'],
             output: {
                 globals: {
                     vue: 'Vue',
-                    protobufjs:'protobufjs',
-                    '@waku/sdk':'@waku/sdk',
-                    '@libp2p/bootstrap':'@libp2p/bootstrap'
+                    protobufjs: 'protobufjs',
+                    '@waku/sdk': '@waku/sdk',
+                    '@libp2p/bootstrap': '@libp2p/bootstrap'
                 },
             },
         },

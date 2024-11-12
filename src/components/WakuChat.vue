58ec37b045b7e2c52d6de6b0006a071612f4fa5d
Vagner <vagner_domingues@hotmail.com>
2024-11-12 10:39:43 -0300

    Fix animDirection in first chat opening

--- src/components/WakuChat.vue
+++ src/components/WakuChat.vue
@@ -397,9 +397,8 @@ defineExpose({openChat, closeChat, connectChat});
 
 <template>
   <div class="waku-chat-vue-plugin">
-    <div v-if="getStatus() === 'connected' || getStatus() === 'connecting'" key="connected">
-      <Transition :name="animDirection()" mode="out-in">
-        <div v-if="isChatOpen" class="chat-container" :class="{ dark: isDark }">
+    <Transition :name="animDirection()" mode="out-in">
+        <div v-if="isChatOpen && (getStatus() === 'connected' || getStatus() === 'connecting')" class="chat-container" :class="{ dark: isDark }">
           <Transition name="fade" mode="out-in">
             <div
               v-if="loadingRoom"
@@ -610,23 +609,24 @@ defineExpose({openChat, closeChat, connectChat});
           </svg>
         </button>
       </Transition>
-    </div>
-    <div v-else key="disconnected">
-      <button @click="openChat" class="load-button" :class="{ dark: isDark }">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
-    </div>
+      <Transition name="fastFade" mode="out-in">
+        <div v-if="getStatus() === 'disconnected'">
+          <button @click="openChat" class="load-button" :class="{ dark: isDark }">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+      </Transition>
   </div>
 </template>
 
